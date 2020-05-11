386b436364f58c61c9256608f0234080abc5f13c
Luigi Quaranta <quarantalgu@gmail.com>
2020-05-11 16:04:16 +0200

    Reproduce model using RandomForestRegressor

--- src/train.py
+++ src/train.py
@@ -15,23 +15,10 @@ input_folder_path = Path('./prepared')
 # Read training dataset
 X_train = pd.read_csv(input_folder_path / 'X_train.csv')
 y_train = pd.read_csv(input_folder_path / 'y_train.csv')
-X_valid = pd.read_csv(input_folder_path / 'X_valid.csv')
-y_valid = pd.read_csv(input_folder_path / 'y_valid.csv')
-
-candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
-
-# Loop to find the ideal tree size from candidate_max_leaf_nodes
-mae_values = []
-for max_leaf_nodes in candidate_max_leaf_nodes:
-    mae_values.append(get_mae(max_leaf_nodes, X_train, X_valid, y_train, y_valid))
-
-# Best value of max_leaf_nodes
-index_of_minimum_mae = mae_values.index(min(mae_values))
-best_tree_size = candidate_max_leaf_nodes[index_of_minimum_mae]
 
 # Specify the model
 # For the sake of reproducibility, I set the `random_state` argument equal to 0
-iowa_model = DecisionTreeRegressor(random_state=0, max_leaf_nodes=best_tree_size)
+iowa_model = RandomForestRegressor(random_state=1)
 
 # Then I fit the model to the training data
 iowa_model.fit(X_train, y_train)
