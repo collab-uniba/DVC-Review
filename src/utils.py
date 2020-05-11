21c4a9bede61034e09d363acb322b28083a0481c
Luigi Quaranta <quarantalgu@gmail.com>
2020-05-11 15:53:15 +0200

    Reproduce model using optimal max_leaf_nodes value

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import os
 import errno
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
 
 def mkdir_p(path):
     try:
@@ -9,3 +11,10 @@ def mkdir_p(path):
             pass
         else:
             raise
+
+def get_mae(max_leaf_nodes, X_train, X_valid, y_train, y_valid):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(X_train, y_train)
+    preds_val = model.predict(X_valid)
+    mae = mean_absolute_error(y_valid, preds_val)
+    return(mae)
